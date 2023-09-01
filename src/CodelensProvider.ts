3dc75060350d8a13a0dde1b889901787f017df13
pit00 <pitu.danilo@gmail.com>
2023-09-01 20:55:27 -0300

    select function

--- src/CodelensProvider.ts
+++ src/CodelensProvider.ts
@@ -6,116 +6,120 @@ import { getScriptToRunCode } from "./MarkdownUtil";
  * and provides commands to operate on those
  */
 export class CodelensProvider implements vscode.CodeLensProvider {
-  private codeLenses: vscode.CodeLens[] = [];
-  private regex: RegExp;
-  private _onDidChangeCodeLenses: vscode.EventEmitter<void> =
-    new vscode.EventEmitter<void>();
-  public readonly onDidChangeCodeLenses: vscode.Event<void> =
-    this._onDidChangeCodeLenses.event;
+    private codeLenses: vscode.CodeLens[] = [];
+    private regex: RegExp;
+    private _onDidChangeCodeLenses: vscode.EventEmitter<void> =
+        new vscode.EventEmitter<void>();
+    public readonly onDidChangeCodeLenses: vscode.Event<void> =
+        this._onDidChangeCodeLenses.event;
 
-  constructor() {
-    this.regex = /```([\s\S].[\s\S]*?[\s\S])```/g;
-    vscode.workspace.onDidChangeConfiguration((_) => {
-      this._onDidChangeCodeLenses.fire();
-    });
-  }
-
-  /**
-   * Provides the code lenses for markdown code blocks
-   * @param document Current opened document
-   * @param token Cancellation Taken
-   * @returns vscode.CodeLens[] list of codelenses
-   */
-  public provideCodeLenses(
-    document: vscode.TextDocument,
-    token: vscode.CancellationToken
-  ): vscode.CodeLens[] | Thenable<vscode.CodeLens[]> {
-    const isMarkdownFile =
-      vscode.window.activeTextEditor?.document.languageId === "markdown";
-    if (isMarkdownFile) {
-      this.codeLenses = [];
-      const regex = new RegExp(this.regex);
-      const text = document.getText();
-      let matches;
-      while ((matches = regex.exec(text)) !== null) {
-        const reg = new RegExp(/(.+)/g);
-        let content: string = matches[0];
-        let codeLines = content.split("\n");
-        codeLines.splice(0, 1);
-        let code = codeLines.join("\n").replace(/`/g, "");
-        // Add Copy Command
-        const line = document.lineAt(document.positionAt(matches.index).line);
-        const copyPosition = new vscode.Position(line.lineNumber, 0);
-        const copyRange = document.getWordRangeAtPosition(
-          copyPosition,
-          new RegExp(reg)
-        );
-        const copyCommand: vscode.Command = {
-          title: "Copy",
-          command: "markdown-copy-code.copycode",
-          arguments: [code, false],
-        };
-        if (copyRange) {
-          this.codeLenses.push(new vscode.CodeLens(copyRange, copyCommand));
-        }
-        const runPosition = new vscode.Position(line.lineNumber, 5);
-        // Add Run Code Command
-        if (
-          !vscode.workspace
-            .getConfiguration("markdown-enhanced-code-block")
-            .get("disableRunButton", true)
-        ) {
-          let codeConfigs = content.split("\n")[0].replace(/`/g, "");
-          let details = {
-            position: runPosition,
-            language: codeConfigs.split("|")[0],
-            org: "",
-          };
-          if (
-            (details.language === "apex" || details.language === "soql") &&
-            codeConfigs.split("|").length > 0
-          ) {
-            details.org = codeConfigs.split("|")[1];
-          }
-
-          if (details.language) {
-            code = getScriptToRunCode(code, details);
-          }
-          const runCommand: vscode.Command = {
-            title: "Run",
-            command: "markdown-copy-code.runcode",
-            arguments: [code, details, false],
-          };
-          const runRange = document.getWordRangeAtPosition(
-            runPosition,
-            new RegExp(reg)
-          );
-          if (runRange) {
-            this.codeLenses.push(new vscode.CodeLens(runRange, runCommand));
-          }
-        }
-
-        // Add Replace Variables Command
-        if (content.indexOf("#") !== -1 && content.indexOf("=") !== -1) {
-          const replacePosition = new vscode.Position(line.lineNumber, 8);
-          const replaceCommand: vscode.Command = {
-            title: "Replace Variables",
-            command: "markdown-copy-code.replace-variables",
-            arguments: [content, runPosition, false],
-          };
-          const replaceRanage = document.getWordRangeAtPosition(
-            replacePosition,
-            new RegExp(reg)
-          );
-          if (replaceRanage) {
-            this.codeLenses.push(
-              new vscode.CodeLens(replaceRanage, replaceCommand)
-            );
-          }
-        }
-      }
-      return this.codeLenses;
+    constructor() {
+        this.regex = /```([\s\S].[\s\S]*?[\s\S])```/g;
+        vscode.workspace.onDidChangeConfiguration((_) => {
+            this._onDidChangeCodeLenses.fire();
+        });
     }
-    return [];
-  }
+
+    /**
+     * Provides the code lenses for markdown code blocks
+     * @param document Current opened document
+     * @param token Cancellation Taken
+     * @returns vscode.CodeLens[] list of codelenses
+     */
+    public provideCodeLenses(
+        document: vscode.TextDocument,
+        token: vscode.CancellationToken
+    ): vscode.CodeLens[] | Thenable<vscode.CodeLens[]> {
+        const isMarkdownFile =
+            vscode.window.activeTextEditor?.document.languageId === "markdown";
+        if (isMarkdownFile) {
+            this.codeLenses = [];
+            const regex = new RegExp(this.regex);
+            const text = document.getText();
+            let matches;
+            while ((matches = regex.exec(text)) !== null) {
+                const reg = new RegExp(/(.+)/g);
+                let content: string = matches[0];
+                let codeLines = content.split("\n");
+                codeLines.splice(0, 1);
+                let code = codeLines.join("\n").replace(/`/g, "");
+                
+                // Copy
+                const line = document.lineAt(document.positionAt(matches.index).line);
+                const copyPosition = new vscode.Position(line.lineNumber, 0);
+                const copyRange = document.getWordRangeAtPosition(
+                    copyPosition,
+                    new RegExp(reg)
+                );
+                const copyCommand: vscode.Command = {
+                    title: "📎",
+                    command: "markdown-copy-code.copycode",
+                    arguments: [code, false]
+                };
+                
+                // Select
+                const selectCommand: vscode.Command = {
+                    title: "🔦",
+                    command: "markdown-copy-code.selectcode",
+                    arguments: [line.lineNumber + 1, codeLines.length - 1, false]
+                };
+                
+                // Add Above Commands
+                if (copyRange) {
+                    this.codeLenses.push(new vscode.CodeLens(copyRange, copyCommand));
+                    this.codeLenses.push(new vscode.CodeLens(copyRange, selectCommand));
+                }
+                
+                
+                // Add Run Code Command
+                const runPosition = new vscode.Position(line.lineNumber, 5);
+                if (!vscode.workspace.getConfiguration("markdown-enhanced-code-block").get("disableRunButton", true)) {
+                    let codeConfigs = content.split("\n")[0].replace(/`/g, "");
+                    let details = {
+                        position: runPosition,
+                        language: codeConfigs.split("|")[0],
+                        org: "",
+                    };
+                    if (
+                        (details.language === "apex" || details.language === "soql") &&
+                        codeConfigs.split("|").length > 0
+                    ) {
+                        details.org = codeConfigs.split("|")[1];
+                    }
+
+                    if (details.language) {
+                        code = getScriptToRunCode(code, details);
+                    }
+                    const runCommand: vscode.Command = {
+                        title: "🧩",
+                        command: "markdown-copy-code.runcode",
+                        arguments: [code, details, false]
+                    };
+                    const runRange = document.getWordRangeAtPosition(runPosition, new RegExp(reg));
+                    if (runRange) {
+                        this.codeLenses.push(new vscode.CodeLens(runRange, runCommand));
+                    }
+                }
+
+                // Add Replace Variables Command
+                if (content.indexOf("#") !== -1 && content.indexOf("=") !== -1) {
+                    const replacePosition = new vscode.Position(line.lineNumber, 8);
+                    const replaceCommand: vscode.Command = {
+                        title: "Replace Variables",
+                        command: "markdown-copy-code.replace-variables",
+                        arguments: [content, runPosition, false],
+                    };
+                    const replaceRanage = document.getWordRangeAtPosition(
+                        replacePosition,
+                        new RegExp(reg)
+                    );
+                    if (replaceRanage) {
+                        // this.codeLenses.push(new vscode.CodeLens(replaceRanage, replaceCommand));
+                    }
+                }
+            }
+            return this.codeLenses;
+        }
+        return [];
+    }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -7,108 +7,123 @@ import { CodelensProvider } from "./CodelensProvider";
 
 // extensions is activated whennever a markdown file is opened in the editor
 export function activate(context: vscode.ExtensionContext) {
-  const codelensProvider = new CodelensProvider();
-  // Register the codelense provider
-  vscode.languages.registerCodeLensProvider("*", codelensProvider);
+    const codelensProvider = new CodelensProvider();
+    // Register the codelense provider
+    vscode.languages.registerCodeLensProvider("*", codelensProvider);
 
-  // handle markdown-copy-code.copycode command
-  vscode.commands.registerCommand(
-    "markdown-copy-code.copycode",
-    async (content: any) => {
-      await vscode.env.clipboard.writeText(`${content}`);
-      vscode.window.showInformationMessage(`Copy is successful.`);
-    }
-  );
-
-  // handle markdown-copy-code.runcode command
-  vscode.commands.registerCommand(
-    "markdown-copy-code.runcode",
-    async (content: any, details: any) => {
-      let terminal: any = vscode.window.activeTerminal;
-      if (!terminal) {
-        terminal = await vscode.window.createTerminal("Code Runner");
-      }
-      if (details.language === "apex" || details.language === "soql") {
-        if (!details.org) {
-          vscode.window.showErrorMessage(
-            "Org is not passed, Please pass org like ```" +
-              details.language +
-              "|<sfdx-org-alias>"
-          );
-          return;
+    // Copy
+    vscode.commands.registerCommand(
+        "markdown-copy-code.copycode",
+        async (content: any) => {
+            await vscode.env.clipboard.writeText(`${content}`);
+            vscode.window.showInformationMessage(`Copy is successful.`);
         }
+    );
+    
+    // Select
+    vscode.commands.registerCommand("markdown-copy-code.selectcode", async (startLin: any, totalLin: any) => {
+        vscode.window.activeTextEditor.selection = new vscode.Selection(startLin + totalLin, 0, startLin, 0)
+        // bypasses .selections (that must recive a [ ] of new Selection?) [endLin, endCol, startLin, startCol]
+    });
+    
+    // handle markdown-copy-code.copycode command
+    vscode.commands.registerCommand(
+        "markdown-copy-code.copycode",
+        async (content: any) => {
+            await vscode.env.clipboard.writeText(`${content}`);
+            vscode.window.showInformationMessage(`Copy is successful.`);
+        }
+    );
 
-        if (details.language === "apex") {
-          let typeOfOrg = context.globalState.get(
-            "markdown-enhanced-code-block-org-" + details.org
-          );
-          if (!typeOfOrg) {
-            const isOrgSandbox = await isSandbox(details.org);
-            if (isOrgSandbox) {
-              typeOfOrg = "Sandbox";
-            } else {
-              typeOfOrg = "Production";
+    // handle markdown-copy-code.runcode command
+    vscode.commands.registerCommand(
+        "markdown-copy-code.runcode",
+        async (content: any, details: any) => {
+            let terminal: any = vscode.window.activeTerminal;
+            if (!terminal) {
+                terminal = await vscode.window.createTerminal("Code Runner");
             }
-            context.globalState.update(
-              "markdown-enhanced-code-block-org-" + details.org,
-              typeOfOrg
-            );
-          }
-          if (typeOfOrg === "Production") {
-            let confirmation = await vscode.window.showInformationMessage(
-              "You are running this on production, are you sure?",
-              "Yes",
-              "No"
-            );
-            if (confirmation !== "Yes") {
-              return;
-            }
-          }
-        }
-      }
-      terminal.show();      
-      if(vscode.workspace
-          .getConfiguration("markdown-enhanced-code-block")
-          .get("clearTerminalBeforeRun", true)){
-        vscode.commands.executeCommand("workbench.action.terminal.clear");        
-      }      
-      terminal.sendText(`${content}`);
-    }
-  );
+            if (details.language === "apex" || details.language === "soql") {
+                if (!details.org) {
+                    vscode.window.showErrorMessage(
+                        "Org is not passed, Please pass org like ```" +
+                            details.language +
+                            "|<sfdx-org-alias>"
+                    );
+                    return;
+                }
 
-  // handle markdown-copy-code.replace-variables command
-  vscode.commands.registerCommand(
-    "markdown-copy-code.replace-variables",
-    async (content: string, position: vscode.Position) => {
-      let currentDocumentURI = vscode.window.activeTextEditor?.document.uri;
-      if (currentDocumentURI) {
-        content = replaceVariables(content);
-        let textEncoder: TextEncoder = new TextEncoder();
-        if (vscode.workspace.workspaceFolders) {
-          let tempFileUri = vscode.Uri.file(
-            vscode.workspace.workspaceFolders[0]?.uri.fsPath +
-              "/markdown-enhanced-code-block-temp.md"
-          );
-          vscode.workspace.fs.writeFile(
-            tempFileUri,
-            textEncoder.encode(content)
-          );
-          // show the code
-          let location = new vscode.Location(
-            tempFileUri,
-            new vscode.Position(0, 0)
-          );
-          await vscode.commands.executeCommand(
-            "editor.action.peekLocations",
-            currentDocumentURI,
-            position,
-            [location],
-            "peek"
-          );
+                if (details.language === "apex") {
+                    let typeOfOrg = context.globalState.get(
+                        "markdown-enhanced-code-block-org-" + details.org
+                    );
+                    if (!typeOfOrg) {
+                        const isOrgSandbox = await isSandbox(details.org);
+                        if (isOrgSandbox) {
+                            typeOfOrg = "Sandbox";
+                        } else {
+                            typeOfOrg = "Production";
+                        }
+                        context.globalState.update(
+                            "markdown-enhanced-code-block-org-" + details.org,
+                            typeOfOrg
+                        );
+                    }
+                    if (typeOfOrg === "Production") {
+                        let confirmation = await vscode.window.showInformationMessage(
+                            "You are running this on production, are you sure?",
+                            "Yes",
+                            "No"
+                        );
+                        if (confirmation !== "Yes") {
+                            return;
+                        }
+                    }
+                }
+            }
+            terminal.show();
+            if(vscode.workspace
+                    .getConfiguration("markdown-enhanced-code-block")
+                    .get("clearTerminalBeforeRun", true)){
+                vscode.commands.executeCommand("workbench.action.terminal.clear");
+            }
+            terminal.sendText(`${content}`);
         }
-      }
-    }
-  );
+    );
+
+    // handle markdown-copy-code.replace-variables command
+    vscode.commands.registerCommand(
+        "markdown-copy-code.replace-variables",
+        async (content: string, position: vscode.Position) => {
+            let currentDocumentURI = vscode.window.activeTextEditor?.document.uri;
+            if (currentDocumentURI) {
+                content = replaceVariables(content);
+                let textEncoder: TextEncoder = new TextEncoder();
+                if (vscode.workspace.workspaceFolders) {
+                    let tempFileUri = vscode.Uri.file(
+                        vscode.workspace.workspaceFolders[0]?.uri.fsPath +
+                            "/markdown-enhanced-code-block-temp.md"
+                    );
+                    vscode.workspace.fs.writeFile(
+                        tempFileUri,
+                        textEncoder.encode(content)
+                    );
+                    // show the code
+                    let location = new vscode.Location(
+                        tempFileUri,
+                        new vscode.Position(0, 0)
+                    );
+                    await vscode.commands.executeCommand(
+                        "editor.action.peekLocations",
+                        currentDocumentURI,
+                        position,
+                        [location],
+                        "peek"
+                    );
+                }
+            }
+        }
+    );
 }
 
 // This method is called when your extension is deactivated
